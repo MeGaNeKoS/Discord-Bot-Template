96426b222be731c9c5a5bb2c0cbd04caf20de8b2
MeGaNeKo <evictory90@gmail.com>
2022-01-13 01:53:12 +0700

    Add autoload all method tasks.Loop

--- cogs/task/background.py
+++ cogs/task/background.py
@@ -4,12 +4,16 @@ Description:
 This is a template to create your own discord bot in python.
 Version: 1.0
 """
-from nextcord.ext import commands
+from nextcord.ext import commands, tasks
 
 
 class Auto(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        # Start all method from self, where it callable, and it a instance of tasks.loop
+        for func in self.__dir__():
+            if callable(task := getattr(self, func)) and isinstance(task, tasks.Loop):
+                task.start()
 
     # @tasks.loop()
     # async def spam(self):
