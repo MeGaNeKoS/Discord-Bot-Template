dbb289e1612f5e4eed2ce65afd852f83744c164d
MeGaNeKo <evictory90@gmail.com>
2022-04-14 12:54:53 +0700

    Add: add feature the bot will responses with help command on pinged

--- cogs/events/clients.py
+++ cogs/events/clients.py
@@ -4,7 +4,12 @@ Description:
 This is a template to create your own discord bot in python.
 Version: 1.0
 """
+import asyncio
+
+import nextcord
 from nextcord.ext import commands, tasks
+
+from utils import helpers
 
 
 class Client(commands.Cog):
@@ -26,6 +31,20 @@ class Client(commands.Cog):
                     task.start()
 
         print("Bot is ready!")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # Whenever the bot is tagged, respond with its prefix
+        if message.author != self.bot.user and message.content == f"<@{self.bot.user.id}>":
+            embed = helpers.embed(title="Prefix",
+                                  description=f"My prefix is `{self.bot.command_prefix}`",
+                                  colour=nextcord.Colour.random())
+            await message.channel.send(embed=embed, delete_after=15)
+            await asyncio.sleep(15)
+            try:
+                await message.delete()
+            except nextcord.errors.NotFound:
+                pass
 
 
 def setup(bot):
